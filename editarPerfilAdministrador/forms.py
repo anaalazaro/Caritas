17e890b998025046bd738f86f291ad11263c8262
Ana <any.lazaro17@gmail.com>
2024-05-15 15:59:07 -0300

    Se agrega editar perfil (incompleto)

---
+++ editarPerfilAdministrador/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from app.models import CustomUser
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from django.utils.translation import gettext, gettext_lazy as _
+
+class UsuarioIntercambiadorForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['first_name','last_name']
+        labels = {
+            'first_name': _('Nombre'),
+            'last_name': _('Apellido'),
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
