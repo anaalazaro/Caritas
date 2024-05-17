1e7c4b17e716958d3d46289c120ee65e2000490e
zoe <zoezapettini21@gmail.com>
2024-05-17 18:12:22 -0300

    .

--- autenticacionIntercambiador/views.py
+++ autenticacionIntercambiador/views.py
@@ -22,22 +22,13 @@ def login_view(request):
             # Escenario 1: Inicio de sesión exitoso
             if user is not None:
                 login(request, user)
-<<<<<<< HEAD
                 return redirect('menuPrincipal')
-=======
-                return HttpResponse("Inició sesión exitosamente")
->>>>>>> 410bfbfffd61f5190e29bcfd9e265d8f64bd63b3
 
             # Escenario 2 y 3: Fallos por usuario inexistente o contraseña incorrecta
             else:
                 # Intento de usuario inexistente
-<<<<<<< HEAD
                 if not CustomUser.objects.filter(dni=dni).exists() :
                     error_message = "El usuario no existe"
-=======
-                if not Intercambiador.objects.filter(dni=dni).exists():
-                    error_message = "El usuario y/o la contraseña son incorrectos"
->>>>>>> 410bfbfffd61f5190e29bcfd9e265d8f64bd63b3
                 else:
                     error_message = "El usuario y/o la contraseña son incorrectos"
                     
