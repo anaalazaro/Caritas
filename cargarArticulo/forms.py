56cbe2d23e759d4b8c97109f02b9642aa2bd1a78
zoe <zoezapettini21@gmail.com>
2024-05-19 12:11:47 -0300

    Solucion de la carga de articulo

--- cargarArticulo/forms.py
+++ cargarArticulo/forms.py
@@ -2,17 +2,6 @@ from django import forms
 from .models import Articulo
 
 class ArticuloForm(forms.ModelForm):
-    def __init__(self, *args, usuario=None, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.usuario = usuario
-
     class Meta:
         model = Articulo
         fields = ['Foto', 'Categoria', 'Titulo', 'Descripcion', 'Estado']
-
-    def save(self, commit=True):
-        instancia = super().save(commit=False)
-        instancia.usuario = self.usuario
-        if commit:
-            instancia.save()
-        return instancia
--- cargarArticulo/models.py
+++ cargarArticulo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from app.models import CustomUser
 
 class Articulo(models.Model):
-    usuario = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(CustomUser, on_delete=models.CASCADE, db_constraint=False)
     Foto = models.ImageField(upload_to='articulos/')
     Categoria = models.CharField(max_length=100)
     Titulo = models.CharField(max_length=200)
@@ -10,5 +10,3 @@ class Articulo(models.Model):
     Estado = models.CharField(max_length=20)
     pendiente = models.BooleanField(default=True)
     aprobado = models.BooleanField(default=False)
-    def asignar_usuario(self, usuario):
-        self.usuario = usuario
