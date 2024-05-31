3d9daf5e0bda8407ed494433fe301bae90edc381
Thomas <serenathomasruiz@gmail.com>
2024-05-31 15:10:18 -0300

    solucionando error de eliminar

--- eliminarCuenta/views.py
+++ eliminarCuenta/views.py
@@ -2,16 +2,12 @@ from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db import transaction
+from app.models import CustomUser
 
-@login_required
-def delete_account(request):
-    if request.method == 'POST':
-        with transaction.atomic():
-            user = request.user
-            user.delete()
-            messages.success(request,'La cuenta ha sido eliminada exitosamente.')
-            return redirect('Inicio')#pagina de inicio
+from django.contrib import messages
 
-
+def delete_account(request, user_id):
+        usuario = CustomUser.objects.get(pk=user_id)
+        usuario.delete()
+        messages.success(request, 'La cuenta se ha eliminado exitosamente')
     
-# Create your views here.
