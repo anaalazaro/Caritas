d49f0e2af3b43f0ce0857bd2a33c5994b24de3b2
Nicolás Pelaez <nepelaez95@gmail.com>
2024-06-11 18:20:29 -0300

    corrección de eliminación de filiales y ayudantes

--- crearFilial/models.py
+++ crearFilial/models.py
@@ -3,7 +3,7 @@ from app.models import CustomUser
 
 class Filial(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
-    ayudante = models.ForeignKey(CustomUser, on_delete=models.CASCADE, db_constraint=False,null=True, blank=True)
+    ayudante = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, db_constraint=False,null=True, blank=True)
 
     def __str__(self):
         return self.nombre
