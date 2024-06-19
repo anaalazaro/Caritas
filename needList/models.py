07d180ec535518df6d15850499b3a0b75287f09d
zoe <zoezapettini21@gmail.com>
2024-06-19 11:07:14 -0300

    .

--- needList/models.py
+++ needList/models.py
@@ -5,3 +5,4 @@ from cargarArticulo.models import Articulo
 class NeedList(models.Model):
     usuario = models.ForeignKey(CustomUser, on_delete=models.CASCADE, db_constraint=False)
     articulo = models.ForeignKey(Articulo, on_delete=models.CASCADE, db_constraint=False)
+
